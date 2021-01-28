f3a8f6fe7b80fe75c03bdd3859981c529bf94483
hirakumorishima02 <business.ujinchu@gmail.com>
2021-01-29 08:25:20 +0900

    main.c commit

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 #include <time.h>
 
 int ransuu();
-int raderGame(int *ans);
+int raderGame(int ans);
 
 int main(void){
     int ans,count,ques;
     ans = ransuu();
     printf("%d\n", ans);
 
-    count = raderGame(&ans);
+    count = raderGame(ans);
 
     printf("もう1度遊びますか？ はい->0 いいえ->1\n");
     scanf("%d", &ques);
@@ -18,15 +18,13 @@ int main(void){
     while(ques==0){
         ans = ransuu();
         printf("%d\n", ans);
-        count = raderGame(&ans);
+        count = raderGame(ans);
         printf("もう1度遊びますか？ はい->0 いいえ->1\n");
         scanf("%d", &ques);
     }
 
     return 0;
 }
-
-
 
 int ransuu(){
     int i;
@@ -36,16 +34,16 @@ int ransuu(){
     return i;
 }
 
-int raderGame(int *ans){
+int raderGame(int ans){
     int num1,num2,count=1;
 
-    while(num1+num2!=*ans||num1-num2!=*ans) {
+    while(num1+num2!=ans||num1-num2!=ans) {
         printf("位置は？");
         scanf("%d", &num1);
         printf("範囲は？");
         scanf("%d", &num2);
-        if(num1+num2 >= *ans && num1-num2 <= *ans) {
-            if(num2==0 && (num1+num2 == *ans || num1-num2 == *ans)) break;
+        if(num1+num2 >= ans && num1-num2 <= ans) {
+            if(num2==0 && (num1+num2 == ans || num1-num2 == ans)) break;
             printf("ヒット\n");
         } else {
             printf("外れ\n");
